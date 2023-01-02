75d213f63e9f8a0c0eb9152b81b6b5da27031182
dlcgold <dlcgold96@gmail.com>
2023-01-02 18:13:24 +0100

    added vcf/bcf input in scripts

--- script/build_slp.py
+++ script/build_slp.py
@@ -10,26 +10,29 @@ def execute_command(command):
     return True
 
 
-dirname = '../build/' + os.path.dirname(os.path.relpath(__file__))
-currdir = 'tmp/'
-bigrepair_dirname = os.path.join(dirname, "_deps/bigrepair-src")
-shaped_slp_dirname = os.path.join(dirname, "_deps/shaped_slp-build")
-
 
 class extract:
-    def __init__(self, filename, output):
+    def __init__(self, filename, output, vcf):
         self.filename = filename
         self.output = output
+        self.vcf = vcf
 
     def run(self):
         filename = self.filename
         output = self.output
         exe = "tsv_to_single_row.py"
-        command = "python {exe} -i {file} -o {currdir}{out}.txt".format(
+        if not self.vcf:
+            command = "python {exe} -i {file} -o {currdir}{out}.txt".format(
+                exe=os.path.join('.', exe), file=filename, currdir=currdir,
+                out=output)
+            if not execute_command(command):
+                return
+        else:
+            command = "python {exe} -i {file} -o {currdir}{out}.txt -v".format(
             exe=os.path.join('.', exe), file=filename, currdir=currdir,
             out=output)
-        if not execute_command(command):
-            return
+            if not execute_command(command):
+                return
 
 
 class bigrepair:
@@ -69,28 +72,43 @@ class shapedslp:
 def main(argv):
     inputfile = ''
     outputfile = ''
+    vcf = False
+    source = ''
     if not os.path.isdir("tmp"):
         os.mkdir("tmp")
     try:
-        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+        opts, args = getopt.getopt(argv, "hi:o:d:v", ["ifile=", "ofile=", "=sdir"])
     except getopt.GetoptError:
-        print('build_slp.py -i <inputfile> -o <outputfile>')
+        print('build_slp.py -i <inputfile> -o <outputfile> -d <source dir> -v/--vcf')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('build_slp.py -i <inputfile> -o <outputfile>')
+            print('build_slp.py -i <inputfile> -o <outputfile> -d <source dir> -v/--vcf')
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
         elif opt in ("-o", "--ofile"):
             outputfile = arg
-
+        elif opt in ("-d", "--sdir"):
+            source = arg
+        elif opt in ("-v", "--vcf"):
+            vcf = True
+    if source[:1] != "/":
+        source += "/"
+    global dirname
+    dirname = source + os.path.dirname(os.path.relpath(__file__))
+    global currdir
+    currdir = 'tmp/'
+    global bigrepair_dirname
+    bigrepair_dirname = os.path.join(dirname, "_deps/bigrepair-src")
+    global shaped_slp_dirname
+    shaped_slp_dirname = os.path.join(dirname, "_deps/shaped_slp-build")
     outdir = outputfile.split('/')[0] + "/" + "".join(
         outputfile.split('/')[1::-2]) + "/"
     outname = outputfile.split('/')[-1].split('.')[0]
 
     print("extract matrix")
-    matr = extract(inputfile, outname)
+    matr = extract(inputfile, outname, vcf)
     matr.run()
 
     print("end extract matrix \nbegin bigrepair")
--- script/tsv_to_single_row.py
+++ script/tsv_to_single_row.py
@@ -1,50 +1,78 @@
 import sys
 import getopt
+from pysam import VariantFile
 
 
 def main(argv):
     inputfile = ''
     outputfile = ''
+    vcf = False
     try:
-        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+        opts, args = getopt.getopt(argv, "hi:o:v", ["ifile=", "ofile="])
     except getopt.GetoptError:
-        print('tsv_to_single_row.py -i <inputfile> -o <outputfile>')
+        print('tsv_to_single_row.py -i <inputfile> -o <outputfile> -v/--vcf')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('tsv_to_single_row.py -i <inputfile> -o <outputfile>')
+            print('tsv_to_single_row.py -i <inputfile> -o <outputfile> -v/--vcf')
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
         elif opt in ("-o", "--ofile"):
             outputfile = arg
-    with open(inputfile) as f:
+        elif opt in ("-v", "--vcf"):
+            vcf = True
+    if not vcf:
+        with open(inputfile) as f:
+            count = 0
+            with open(outputfile, 'w') as out:
+                col = []
+                print("read file")
+                for line in f:
+                    c = str(count)
+                    print(c, end="\r")
+                    if line.strip().split()[0] == 'TOTAL_SAMPLES:':
+                        break
+                    if count > 1:
+                        # out.write(line.strip().split("\t")[4])
+                        col.append(line.strip().split()[4])
+                    count += 1
+                print("end read file")
+                print("inverse matrix")
+                col = col[::-1]
+                print("end inverse matrix of measures: ")
+                print(len(col[0]))
+                print(len(col))
+                print("begin \"stretch\" matrix")
+                for c in range(0, len(col[0])):
+                    print(c, end="\r")
+                    for r in range(0, len(col)):
+                        out.write(col[r][c])
+                print("end \"stretch\" matrix")
+    else:
+        bcf_in = VariantFile(inputfile)  
+        col = []    
         count = 0
+        for rec in bcf_in.fetch(): 
+            c = str(count)
+            print(c, end="\r")
+            col.append("".join([f"{s['GT'][0]}{s['GT'][1]}" for s in rec.samples.values()]))
+            count += 1
+        print("end read file")
+        print("inverse matrix")
+        col = col[::-1]
+        print("end inverse matrix of measures: ")
+        print(len(col[0]))
+        print(len(col))
+        print("begin \"stretch\" matrix")
         with open(outputfile, 'w') as out:
-            col = []
-            print("read file")
-            for line in f:
-                c = str(count)
-                print(c, end="\r")
-                if line.strip().split()[0] == 'TOTAL_SAMPLES:':
-                    break
-                if count > 1:
-                    # out.write(line.strip().split("\t")[4])
-                    col.append(line.strip().split()[4])
-                count += 1
-            print("end read file")
-            print("inverse matrix")
-            col = col[::-1]
-            print("end inverse matrix of measures: ")
-            print(len(col[0]))
-            print(len(col))
-            print("begin \"stretch\" matrix")
             for c in range(0, len(col[0])):
                 print(c, end="\r")
+                tmp = ""
                 for r in range(0, len(col)):
-                    out.write(col[r][c])
-            print("end \"stretch\" matrix")
-
+                    tmp += col[r][c]
+                out.write(tmp)
+        print("end \"stretch\" matrix")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
